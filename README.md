372494ffe07793ef8a980d12600232940852763f
Hakkyu Kim <hakkyu271@gmail.com>
2022-09-30 15:10:45 +0900

    Update test app

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# window_resize
+# Window Resize
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+A simple flutter-tizen app to test window resize and reposition feature.
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+extension RandomExtension on Random {
+  /// Returns a random number x in range min <= x <= max.
+  int nextIntRange(int min, int max) {
+    if (min > max) {
+      throw ArgumentError("min cannot be larger than max.");
+    }
+    return min + nextInt(max - min + 1);
+  }
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return '${this[0].toUpperCase()}${substring(1)}';
+  }
+}
+
+class Geometry {
+  const Geometry({
+    required this.x,
+    required this.y,
+    required this.width,
+    required this.height,
+  });
+
+  final int x;
+  final int y;
+  final int width;
+  final int height;
+}
+
+enum GeometryType {
+  screen,
+  window,
+}
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Window Resize',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Window Resize Home Page'),
     );
   }
 }
@@ -32,30 +69,54 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final MethodChannel _kChannel = const MethodChannel('tizen/internal/window');
 
-  String _windowWidth = 'Unknown';
-  String _windowHeight = 'Unknown';
-  String _screenWidth = 'Unknown';
-  String _screenHeight = 'Unknown';
+  Geometry? _screen;
+  Geometry? _window;
 
-  Future<void> resize() async {
+  final Random _random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    getGeometry(GeometryType.screen).then((value) => setState(
+          () => _screen = value,
+        ));
+    getGeometry(GeometryType.window).then((value) => setState(
+          () => _window = value,
+        ));
+  }
+
+  Future<void> randomResize() async {
+    final int x = _random.nextIntRange(0, 200);
+    final int y = _random.nextIntRange(0, 200);
+    final int width = _random.nextIntRange(200, _screen!.width);
+    final int height = _random.nextIntRange(200, _screen!.height);
+    await resize(x, y, width, height);
+  }
+
+  Future<void> resize(int x, int y, int width, int height) async {
     await _kChannel.invokeMethod(
-        'setWindowGeometry', {'x': 50, 'y': 100, 'width': 700, 'height': 700});
+      'setWindowGeometry',
+      {
+        'x': x,
+        'y': y,
+        'width': width,
+        'height': height,
+      },
+    );
+    getGeometry(GeometryType.window).then((value) => setState(
+          () => _window = value,
+        ));
   }
 
-  Future<void> getGeometry() async {
-    final dynamic map = await _kChannel.invokeMethod('getWindowGeometry');
-    setState(() {
-      _windowWidth = (map['width'] as int).toString();
-      _windowHeight = (map['height'] as int).toString();
-    });
-  }
-
-  Future<void> getScreenSize() async {
-    final dynamic map = await _kChannel.invokeMethod('getScreenGeometry');
-    setState(() {
-      _screenWidth = (map['width'] as int).toString();
-      _screenHeight = (map['height'] as int).toString();
-    });
+  Future<Geometry> getGeometry(GeometryType type) async {
+    final Map<dynamic, dynamic> map =
+        await _kChannel.invokeMethod('get${type.name.capitalize()}Geometry');
+    return Geometry(
+      x: map['x'] as int? ?? 0,
+      y: map['y'] as int? ?? 0,
+      width: map['width'] as int,
+      height: map['height'] as int,
+    );
   }
 
   @override
@@ -64,21 +125,23 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Screen Size: $_screenWidth $_screenHeight'),
-            Text('Window Size: $_windowWidth $_windowHeight'),
-            TextButton(
-                onPressed: () async => getScreenSize(),
-                child: const Text('getWindowScreenSize')),
-            TextButton(
-                onPressed: () async => await getGeometry(),
-                child: const Text('getWindowGeometry')),
-            TextButton(
-                onPressed: () async => await resize(),
-                child: const Text('Resize')),
+            if (_screen != null)
+              Text(
+                  'Screen Geometry: (${_screen!.x}, ${_screen!.y}) (${_screen!.width}, ${_screen!.height})'),
+            if (_window != null)
+              Text(
+                  'Window Geometry: (${_window!.x}, ${_window!.y}) (${_window!.width}, ${_window!.height})'),
+            ElevatedButton(
+                onPressed: () async => await randomResize(),
+                child: const Text('Random Resize')),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () async => await resize(
+                    _screen!.x, _screen!.y, _screen!.width, _screen!.height),
+                child: const Text('To Full Screen')),
           ],
         ),
       ),
